c7e2244ae390504491c769a6c96ddc39ac82ffd6
Gaspard Anna Bucher <g.a.bucher@midasum.com>
2025-03-29 14:29:46 +0100

    Add more sliders

--- slides/components/Slider.tsx
+++ slides/components/Slider.tsx
@@ -1,17 +1,25 @@
 import * as d3 from "d3";
-import React, { useEffect, useRef, useState } from "react";
+import React, {
+  Dispatch,
+  SetStateAction,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 
 const randomX = d3.randomNormal(0, 1);
 const randomY = d3.randomNormal(0, 1);
 
-const allPoints = Array.from({ length: 1000 }, () => {
+const allPoints = Array.from({ length: 2000 }, () => {
   const x = randomX();
   const y = randomY();
   return [x, y];
 });
 
 export function Slider() {
-  const [numPoints, setSliderValue] = useState(500);
+  const [numPoints, setNumPoints] = useState(600);
+  const [pow, setPow] = useState(0.55);
+  const [mult, setMult] = useState(28);
   const [position, setPosition] = useState({ x: 0, y: 0 });
   const [points, setPoints] = useState(allPoints.slice(0, numPoints));
   const chart = useRef<HTMLDivElement>(null);
@@ -28,7 +36,7 @@ export function Slider() {
 
   useEffect(() => {
     drawChart();
-  }, [position, numPoints, width, height]);
+  }, [position, numPoints, width, height, pow, mult]);
 
   useEffect(() => {
     setPoints(allPoints.slice(0, numPoints));
@@ -38,17 +46,23 @@ export function Slider() {
     const move = (e: MouseEvent) => {
       const c = chart.current?.getBoundingClientRect();
       if (!c) return;
-      setPosition({
-        x: e.clientX - c.left - c.width / 2,
-        y: e.clientY - c.top - c.height / 2,
-      });
+      const x = e.clientX - c.left;
+      const y = e.clientY - c.top;
+      if (x < 0 || y < 0 || x > c.width || y > c.height) {
+        setPosition({ x: 0, y: 0 });
+      } else {
+        setPosition({
+          x: x - c.width / 2,
+          y: y - c.height / 2,
+        });
+      }
     };
     document.addEventListener("mousemove", move);
     return () => document.removeEventListener("mousemove", move);
   }, []);
 
-  const handleSliderChange = (e) => {
-    setSliderValue(Number(e.target.value));
+  const handle = (setter: Dispatch<SetStateAction<number>>) => (e) => {
+    setter(Number(e.target.value));
   };
 
   const drawChart = () => {
@@ -59,12 +73,13 @@ export function Slider() {
       const dx = position.x - xp;
       const dy = position.y - yp;
 
-      const k = Math.sqrt(dx ** 2 + dy ** 2) / 100;
-      if (k === 0) {
+      const k1 = Math.sqrt(dx ** 2 + dy ** 2);
+      if (k1 === 0) {
         return [width / 2 + xp, height / 2 + yp];
       }
+      const k = mult / k1 ** pow;
 
-      return [width / 2 + xp + dx / k, height / 2 + yp + dy / k];
+      return [width / 2 + xp + dx * k, height / 2 + yp + dy * k];
     });
     const delaunay = d3.Delaunay.from(cpoints);
     const voronoi = delaunay.voronoi([0, 0, width, height]);
@@ -102,15 +117,42 @@ export function Slider() {
 
   return (
     <div className="slider">
-      <input
-        type="range"
-        min="1"
-        step={1}
-        max={1000}
-        value={numPoints}
-        onChange={handleSliderChange}
-      />
-      <div ref={chart} id="chart" className="flex"></div>
+      <div className="grid grid-cols-2 gap-2">
+        <div>nb points {numPoints}</div>
+        <div>
+          <input
+            type="range"
+            min="1"
+            step={1}
+            max={2000}
+            value={numPoints}
+            onChange={handle(setNumPoints)}
+          />
+        </div>
+        <div>pow {pow.toFixed(2)}</div>
+        <div>
+          <input
+            type="range"
+            min={0}
+            step={0.001}
+            max={6}
+            value={Math.exp(pow)}
+            onChange={handle((v) => setPow(Math.log(v)))}
+          />
+        </div>
+        <div>mult {mult.toFixed(1)}</div>
+        <div>
+          <input
+            type="range"
+            min={-100}
+            step={0.1}
+            max={100}
+            value={mult}
+            onChange={handle(setMult)}
+          />
+        </div>
+      </div>
+      <div ref={chart} id="chart" className="flex cursor-none"></div>
     </div>
   );
 }
